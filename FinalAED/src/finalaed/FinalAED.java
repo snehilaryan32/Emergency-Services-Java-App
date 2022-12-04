a41fe49c943420e6246d4f4135db36ae7afc90ab
xpadyal <padyal.s@northeastern.edu>
2022-12-04 13:04:44 -0500

    testcommit

--- FinalAED/src/finalaed/FinalAED.java
+++ FinalAED/src/finalaed/FinalAED.java
@@ -2,19 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package crimepatrolfinalproject;
+package finalaed;
 
 /**
  *
- * @author aryan
+ * @author sahilpadyal
  */
-public class CrimePatrolFinalProject {
+public class FinalAED {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello moshaa!");
+        
     }
     
 }
