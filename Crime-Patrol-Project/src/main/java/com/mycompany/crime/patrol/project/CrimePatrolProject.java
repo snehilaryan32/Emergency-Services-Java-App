ffd0e9e55f3e85f6d4d9cbb3303dc55e6e0a6c0c
xpadyal <padyal.s@northeastern.edu>
2022-12-04 13:13:08 -0500

    New Start

---
+++ Crime-Patrol-Project/src/main/java/com/mycompany/crime/patrol/project/CrimePatrolProject.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.crime.patrol.project;
+
+/**
+ *
+ * @author sahilpadyal
+ */
+public class CrimePatrolProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
