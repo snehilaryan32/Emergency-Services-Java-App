251037a05c55e553d03035fbb7b7d9e5910700cb
snehil.aryan <aryan.s@northeastern.edu>
2022-12-04 16:04:42 -0500

    CHange

--- Crime-Patrol-Project/src/main/java/com/mycompany/crime/patrol/project/CrimePatrolProject.java
+++ Crime-Patrol-Project/src/main/java/com/mycompany/crime/patrol/project/CrimePatrolProject.java
@@ -12,6 +12,6 @@ package com.mycompany.crime.patrol.project;
 public class CrimePatrolProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World From Windows ");
     }
 }
