b7f9cdad4075dc01113c684142c4660e1a3f263f
bashubisht <98195578+bashubisht@users.noreply.github.com>
2022-12-09 00:59:33 -0500

    Update MainMapSysAdmin.java

--- CrimePatrolFinalProject/src/crimepatrolfinalproject/MainMapSysAdmin.java
+++ CrimePatrolFinalProject/src/crimepatrolfinalproject/MainMapSysAdmin.java
@@ -183,7 +183,7 @@ public class MainMapSysAdmin extends javax.swing.JFrame {
         });
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                jButton1ActionPerformed(evt);
             }
         });
 
